31009901d21b13c8acb9b7814531b26599e4b2bc
evilaaron11 <aaron.m.a@comcast.net>
2016-06-23 15:20:45 -0400

    Game works but bet system is needed

--- Game.py
+++ Game.py
@@ -5,6 +5,7 @@ from Card import *
 FACE_VALUE = 10
 ACE = 11
 MAX_VALUE = 21
+HOUSE_STAY = 17
 
 class Game:
    # Variables to keep track of current deck and hands
@@ -21,12 +22,13 @@ class Game:
          self.player.append(self.deck.drawCard())
          self.house.append(self.deck.drawCard())
 
-   def checkHand(cards):
+   def checkHand(self, cards):
       score = 0
       for i in cards:
-         if cards.val.isdigit():
-            score += cards.val
-         elif cards.val == "A":
+         currVal = str(i.val)
+         if currVal.isdigit():
+            score += i.val
+         elif i.val == "A":
             if MAX_VALUE > score + ACE:
                score += ACE
             else:
@@ -39,13 +41,51 @@ class Game:
    def hitMe(self):
       self.player.append(self.deck.drawCard())
 
-   def checkBust(score):
+   def hitDealer(self):
+      self.house.append(self.deck.drawCard())
+
+   def checkBust(self, score):
       return score > MAX_VALUE
 
    def printHand(self):
       for i in self.player:
          print i
 
+   def printDealer(self):
+      for j in self.house:
+         print j
+
    #run AI
    def runHouse(self):
-      print "Not yet implemented"
+      print "Dealer\'s hand:"
+      self.printDealer()
+
+      playerScore = self.checkHand(self.player)
+      houseScore = self.checkHand(self.house)
+
+      print "House score: " + str(houseScore)
+      while houseScore < HOUSE_STAY:
+         print "Dealer took a hit"
+         self.hitDealer()
+         self.printDealer()
+         houseScore = self.checkHand(self.house)
+
+      if self.checkIfWon(False):
+         print "You won"
+      else:
+         print "You lose"
+
+   def checkIfWon(self, beforeHouse):
+      playerScore = self.checkHand(self.player)
+      houseScore = self.checkHand(self.house)
+
+      if beforeHouse:
+         if playerScore == MAX_VALUE:
+            print "You have 21"
+            return True
+         else:
+            return False
+      else:
+         return playerScore > houseScore
+
+
--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ from Game import *
 import sys
 
 def main():
+   beforeHouse = True
    deck = Deck()
    #deck.printCards()
    inProgress = True
@@ -12,18 +13,31 @@ def main():
    currGame = Game(deck)
    currGame.printHand()
    
+   if currGame.checkIfWon(beforeHouse):
+      return
+   print "The dealers top card is a " + str(currGame.house[0])
+
    while inProgress:
+      score = currGame.checkHand(currGame.player)
+      print "Current hand: " + str(score)
+      if currGame.checkBust(score):
+         print "You have busted. You lose."
+         break
+      elif currGame.checkIfWon(beforeHouse):
+         currGame.runHouse()
+         break
+
       input = raw_input()
       if input == "q":
          inProgress = False
       elif input == "h":
          currGame.hitMe()
          currGame.printHand()
-      elif input == "s":
+      elif input == "s": 
          currGame.runHouse()
+         beforeHouse = False
+         break
       else:
          print "Invalid input"
-
-      
 
 if __name__ == "__main__": main()
